9f9cce8b2c260d137eb546dd721ab8119761e19e
marianitadileo <mdileo36@gmail.com>
2023-03-13 19:52:25 +0100

    add rnd numbers

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,4 +1,30 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-const rndNumb = Math.floor(Math.random() * 6) + 1
+console.log("VINCE CHI FA IL PUNTEGGIO MAGGIORE");
+
+const nameUser = prompt("inserisci il tuo nome!");
+
+// genero un numero casuale
+const rndNumbPlayer = Math.floor(Math.random() * 6) + 1;
+console.log(nameUser + " il tuo numero è " + rndNumbPlayer);
+const rndNumb = Math.floor(Math.random() * 6) + 1;
+console.log("il numero del computer è " + rndNumb);
+
+// stabilisco il vincitore 
+if (rndNumb > rndNumbPlayer) {
+    console.log("vince il computer!");
+} else if (rndNumbPlayer > rndNumb) {
+    console.log(`vince ${nameUser}`);
+} else {
+    console.log("pareggio");
+}
+
+document.getElementById('numb').innerHTML = (nameUser + " il tuo numero è " + rndNumbPlayer);
+document.getElementById('numb2').innerHTML = ("il numero del computer è " + rndNumb);
+
+
+
+
+
+
