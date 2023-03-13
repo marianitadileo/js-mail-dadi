fdc3122f4be90166367aef0ea179cfc99eb855c0
marianitadileo <mdileo36@gmail.com>
2023-03-13 23:20:16 +0100

    add exercise

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -11,17 +11,25 @@ console.log(nameUser + " il tuo numero è " + rndNumbPlayer);
 const rndNumb = Math.floor(Math.random() * 6) + 1;
 console.log("il numero del computer è " + rndNumb);
 
+document.getElementById('numb').innerHTML = (nameUser + " il tuo numero è " + rndNumbPlayer);
+document.getElementById('numb2').innerHTML = ("il numero del computer è " + rndNumb);
+
+let winner = "";
+
 // stabilisco il vincitore 
 if (rndNumb > rndNumbPlayer) {
-    console.log("vince il computer!");
+    winner = "il vincitore è il computer!";
+    console.log("il vincitore è il computer!");
 } else if (rndNumbPlayer > rndNumb) {
+    winner = `il vincitore è ${nameUser}`;
     console.log(`vince ${nameUser}`);
 } else {
+    winner = "pareggio";
     console.log("pareggio");
 }
 
-document.getElementById('numb').innerHTML = (nameUser + " il tuo numero è " + rndNumbPlayer);
-document.getElementById('numb2').innerHTML = ("il numero del computer è " + rndNumb);
+document.getElementById('winner').innerHTML = (`${winner}`);
+
 
 
 
